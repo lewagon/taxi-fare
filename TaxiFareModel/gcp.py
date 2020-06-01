c3227cd0f681fb9bd9a427ab7bf8595de64520de
lologibus2 <jea.bizot@gmail.com>
2020-06-01 10:06:24 +0200

    added get_model from gcp

--- TaxiFareModel/gcp.py
+++ TaxiFareModel/gcp.py
@@ -20,3 +20,20 @@ def storage_upload(model_directory, bucket=BUCKET_NAME, rm=False):
                   "green"))
     if rm:
         os.remove('model.joblib')
+
+def download_model(model_directory="PipelineTest", bucket=BUCKET_NAME, rm=True):
+    creds = get_credentials()
+    client = storage.Client(credentials=creds, project=PROJECT_ID).bucket(bucket)
+
+    storage_location = '{}/{}/{}/{}'.format(
+        'models',
+        'taxi_fare_model',
+        model_directory,
+        'model.joblib')
+    blob = client.blob(storage_location)
+    blob.download_to_filename('model.joblib')
+    print(f"=> pipeline downloaded from storage")
+    model = joblib.load('model.joblib')
+    if rm:
+        os.remove('model.joblib')
+    return model
